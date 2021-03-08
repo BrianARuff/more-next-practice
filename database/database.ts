7a0483fe3af216148b264653c5c26050297b58e2
BrianARuff <briananthonyruff@gmail.com>
2021-03-07 23:56:45 -0500

    set up db pool based on env

--- database/database.ts
+++ database/database.ts
@@ -4,13 +4,19 @@ dotenv.config({path: __dirname + "../.env"});
 
 import {Pool} from "pg";
 
-const database = new Pool({
-  user: process.env.PG_USERNAME,
-  password: process.env.PG_PASSWORD,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  port: Number(process.env.PG_PORT),
-});
+let database;
+
+if (process.env.NODE_ENV === "production") {
+  database = new Pool({connectionString: "Connection String"});
+} else {
+  database = new Pool({
+    user: process.env.PG_USERNAME,
+    password: process.env.PG_PASSWORD,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    port: Number(process.env.PG_PORT),
+  });
+}
 
 database.connect();
 
